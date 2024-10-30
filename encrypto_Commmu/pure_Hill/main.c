a7c49304d7907691a4fe5f4b4f3aa33e449f95a6
supergravityy <dbtjdtn325@gmail.com>
2024-10-31 00:23:21 +0900

    pureHill : second

--- encrypto_Commmu/pure_Hill/main.c
+++ encrypto_Commmu/pure_Hill/main.c
@@ -4,17 +4,34 @@
 
 int* make_keyArray(int* key_size)
 {
+	/*--------------------------*/
+	// 1. 키 행렬 사이즈 입력받기
+	/*--------------------------*/
+
 	int* keyArray = NULL;
 	printf("키의 크기를 입력하세요:");
 	scanf("%d", key_size);
 	printf("\n");
 
-	if ((key_size > 0))
+	if (!(key_size > 0))
 	{
-
+		goto make_keyArray_exit;
 	}
-	keyArray = (int*)malloc((*key_size) * (*key_size) * sizeof(int));
 
+	/*--------------------------*/
+	// 2. 키행렬 할당
+	/*--------------------------*/
+	
+	keyArray = (int*)malloc((int)pow(*key_size, 2) * sizeof(int));
+
+	if (keyArray == NULL)
+	{
+		goto make_keyArray_exit;
+	}
+
+	// 혹시몰라 더 써질지?
+
+make_keyArray_exit:
 	return keyArray;
 }
 
@@ -23,13 +40,18 @@ void keyElement_input(int* keyArray, int key_size)
 	
 }
 
-
-
 void main(void)
 {
 	int key_size = 0;
 	int* keyArr = make_keyArray(&key_size);
-	
 
+	if (keyArr == NULL)
+	{
+		goto main_exit;
+	}
+
+	keyElement_input(keyArr, key_size);
+
+main_exit:
 	return;
 }
